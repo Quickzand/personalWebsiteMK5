482d7401ff591199b219a4bdc973f9aece624fa4
Matthew Sand <matthew@Mattbook.local>
2022-02-25 01:49:57 -0500

    Fixed some bugs, added meta tags, and favicon

--- js/main.js
+++ js/main.js
@@ -68,3 +68,15 @@ $.fn.isInViewport = function () {
     var viewportBottom = viewportTop + $(window).height();
     return elementBottom > viewportTop && elementTop < viewportBottom;
 };
+
+
+// Constructs the meta tags and icon for the webpage
+function metaTagsBuilder() {
+    var author = $("<meta>").attr("name", "author").attr("content", "Matthew Sand");
+    var description = $("<meta>").attr("name", "description").attr("content", "Fullstack Web Developer and Computer Science Student");
+    var keywords = $("<meta>").attr("name", "keywords").attr("content", "Resume, Fullstack, Developer, Computer Science, University, Central, Florida, Matthew, Sand, Matthew Sand, Programmer");
+    var viewport = $("<meta>").attr("name", "viewport").attr("content", "width=device-width, initial-scale=1.0");
+    var icon = $("<link>").attr("rel", "icon").attr("href", "favicon.ico");
+
+    $("head").append(author).append(description).append(keywords).append(viewport).append(icon);
+}
